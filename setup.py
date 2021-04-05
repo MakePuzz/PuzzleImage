abe8e08710fab8ad140332306894c70dca5e4c7b
YuyaMatsu <kraimdeenr01241@gmail.com>
2021-04-05 12:29:39 +0900

    v0.0.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def _requires_from_file(filename):
 extensions = []
 setup(
     name="puzzleimage",
-    version="0.0.5",
+    version="0.0.6",
     url='https://github.com/MakePuzz/puzzleimage',
     author='The puzzle-japan Team',
     author_email='puzzle.hokkaido@gmail.com',
